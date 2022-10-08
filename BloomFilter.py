6410f426d427a5dbc307c29cf3f1570b65734f38
Sean <seankan@DESKTOP-O7JO25I>
2022-10-08 16:56:44 -0400

    Modified

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,8 +1,3 @@
-
-# Python 3 program to build Bloom Filter
-# Install mmh3 and bitarray 3rd party module first
-# pip install mmh3
-# pip install bitarray
 import math
 import mmh3
 from bitarray import bitarray
@@ -20,13 +15,7 @@ class BloomFilter(object):
             Size of the bit array in bloom filter
         hash_count : int
             number of hash function to be used
-        '''
-        # # False possible probability in decimal
-        # self.fp_prob = fp_prob
-  
-        # # Size of bit array to use
-        # self.bitarfray_size = self.get_size(items_count, fp_prob)
-  
+        ''' 
         # number of hash functions to use
         self.hash_count = hash_count
   
@@ -42,9 +31,9 @@ class BloomFilter(object):
 
 
   
-    def add(self, item):
+    def insert_str(self, item):
         '''
-        Add an item in the filter
+        Add a string in the filter
         '''
         digests = []
         for i in range(self.hash_count):
@@ -58,9 +47,9 @@ class BloomFilter(object):
             # set the bit True in bit_array
             self.bit_array[digest] = True
   
-    def check(self, item):
+    def testmembership_str(self, item):
         '''
-        Check for existence of an item in filter
+        Check for existence of a string in filter
         '''
         for i in range(self.hash_count):
             digest = mmh3.hash(item, i) % self.size
@@ -72,31 +61,8 @@ class BloomFilter(object):
                 return False
         return True
   
-    @classmethod
-    def get_size(self, n, p):
+    def reset(self):
         '''
-        Return the size of bit array(m) to used using
-        following formula
-        m = -(n * lg(p)) / (lg(2)^2)
-        n : int
-            number of items expected to be stored in filter
-        p : float
-            False Positive probability in decimal
+        reset the bitarray of the bloomfilter
         '''
-        m = -(n * math.log(p))/(math.log(2)**2)
-        return int(m)
-  
-    @classmethod
-    def get_hash_count(self, m, n):
-        '''
-        Return the hash function(k) to be used using
-        following formula
-        k = (m/n) * lg(2)
-  
-        m : int
-            size of bit array
-        n : int
-            number of items expected to be stored in filter
-        '''
-        k = (m/n) * math.log(2)
-        return int(k)
+        self.bit_array.setall(0)
